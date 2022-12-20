5836d1a1017aa7444296d12d2bce0dc53e3470c3
B5WANG <neo.b5wang@gmail.com>
2022-12-20 21:36:50 +0800

    java coding exercise
    java coding exercise

---
+++ src/main/java/com/b5wang/javabasics/leetcode/entrylevel/Ex217_FindDuplicatedElements.java
@@ -0,0 +1,32 @@
+package com.b5wang.javabasics.leetcode.entrylevel;
+
+/**
+ * https://leetcode.cn/problems/contains-duplicate/?envType=study-plan&id=shu-ju-jie-gou-ru-men&plan=data-structures&plan_progress=yvcyv1t
+ *
+ * 217. 存在重复元素
+ * Find out repeated integers in an array.
+ *
+ * */
+public class Ex217_FindDuplicatedElements {
+
+    /**
+     * 简单粗暴法：分别比较各个数字
+     * 1. 第一个数字和后面的数字比较
+     * 2. 第二个数字和后面的数字比较
+     * ...
+     * n-1. 和最后一个数字比较
+     * 时间复杂度是 O(n*n)
+     * */
+
+
+
+    /**
+     * 要想再提高速度，目前我能想到的就是先排序，在比较相邻的两个数字打小
+     * 需要用一个快的排序算法。
+     * */
+
+    public static void main(String[] args){
+
+    }
+
+}
