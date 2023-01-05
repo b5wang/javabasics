07986be9f9cc6a7f90056f185cbde9b4cc886c11
B5WANG <neo.b5wang@gmail.com>
2023-01-06 00:23:23 +0800

    217 and 51
    217 and 51

--- src/main/java/com/b5wang/javabasics/leetcode/entrylevel/Ex217_FindDuplicatedElements.java
+++ src/main/java/com/b5wang/javabasics/leetcode/entrylevel/Ex217_FindDuplicatedElements.java
@@ -22,33 +22,59 @@ public class Ex217_FindDuplicatedElements {
 
 
 
+
     /**
      * 要想再提高速度，目前我能想到的就是先排序，在比较相邻的两个数字打小
      * 需要用一个快的排序算法。
+     * TODO Sorting algorithm
      * */
+
+
+
 
 
     /** Fastest.
-     *  Similar to HashMap, searching very fast
+     *  1. Similar to HashMap, searching very fast
+     *  2. Consider negative integer
      * */
     public boolean containsDuplicate(int[] nums) {
-
-        if(nums.length == 1){
+        int len = nums.length;
+        if(len == 1){
             return false;
         }
 
+        if(len == 2){
+            return (nums[0] == nums[1]);
+        }
 
         IntNode[] bucket = new IntNode[nums.length];
-
-        int len = nums.length;
         for(int i=0; i<len; i++){
             int index = nums[i] % len;
+            if(index < 0){
+                index = -1 * index;
+            }
             IntNode node = bucket[index];
             if(node == null){
+                // no node yet
                 node = new IntNode(nums[i]);
                 bucket[index] = node;
             }else{
+                // exist some nodes
+                // 1st node
+                if(node.num == nums[i]){
+                    return true;
+                }
 
+                // 2nd to last node
+                while(node.next != null){
+                    node = node.next;
+                    if(node.num == nums[i]){
+                        return true;
+                    }
+                }
+
+                // add new num to node link end
+                node.next = new IntNode(nums[i]);
             }
         }
 
@@ -67,6 +93,15 @@ public class Ex217_FindDuplicatedElements {
 
     public static void main(String[] args){
 
+        int[] nums1 = {1,2,3,1};
+        int[] nums2 = {1,2,3,4};
+        int[] nums3 = {1,1,1,3,3,4,3,2,4,2};
+
+        Ex217_FindDuplicatedElements ex217 = new Ex217_FindDuplicatedElements();
+
+        System.out.println(ex217.containsDuplicate(nums1));
+        System.out.println(ex217.containsDuplicate(nums2));
+        System.out.println(ex217.containsDuplicate(nums3));
     }
 
 }
---
+++ src/main/java/com/b5wang/javabasics/leetcode/entrylevel/Ex51_maxSubArray.java
@@ -0,0 +1,29 @@
+package com.b5wang.javabasics.leetcode.entrylevel;
+
+public class Ex51_maxSubArray {
+
+    /**
+     * ?
+     * */
+
+
+
+    /**
+     * ?
+     * */
+
+
+
+    public int maxSubArray(int[] nums) {
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+
+
+}
