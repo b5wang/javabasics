a29c707ae1d6083bf5c9ee02b84d58a2e3967452
B5WANG <neo.b5wang@gmail.com>
2023-01-19 00:08:29 +0800

    remove repeated elements in array 2
    remove repeated elements in array 2 -  double pointers

--- src/main/java/com/b5wang/javabasics/alg/primary/array/RemoveRepeatedInSortedArray.java
+++ src/main/java/com/b5wang/javabasics/alg/primary/array/RemoveRepeatedInSortedArray.java
@@ -13,7 +13,7 @@ public class RemoveRepeatedInSortedArray {
      *    Move all elements forward 1 space.
      * */
     public int[] remove(int[] nums, int index){
-        System.out.println("Before: " + Arrays.toString(nums));
+        //System.out.println("remove - Before: " + Arrays.toString(nums));
 
         for(int i = index; i < nums.length; i++){
             if(i+1 < nums.length){
@@ -23,55 +23,69 @@ public class RemoveRepeatedInSortedArray {
             }
         }
 
-        System.out.println("After: " + Arrays.toString(nums));
+        //System.out.println("remove - After: " + Arrays.toString(nums));
         return nums;
     }
 
 
 
+    /**
+     * 简单暴力直接解法。先解决有没有，再解决行不行
+     * 把算法拆解，
+     * 1，找相同的相邻元素
+     * 2，如果arr[i-1] = arr[i] 位置的元素，将i后面的元素依次向前移动一个位置，覆盖元素arr[i]
+     * 3. 后面多出来数字标记为arr[0]-1, 数组中最小的数字
+     * */
     public int removeDuplicates(int[] nums) {
-        System.out.println("Before: " + Arrays.toString(nums));
+        //System.out.println("Before: " + Arrays.toString(nums));
         int len = nums.length;
 
-        if(len == 1){
-            return 1;
-        }
-
+        // Start from 2nd element
         int i = 1;
         while(i < len){
             if(nums[i-1] == nums[i]){
-                if(i + 1 == len){
-                    // nums[i] is the last element in array.
-                    // nums[0] is the smallest element in array.
+                if(i == len - 1){
+                    // nums[i] is the last element in array, don't need move the other elements
+                    // Mark nums[0] is the smallest element in array.
                     nums[i] = nums[0] - 1;
                 }else{
-                    // nums[i] is not the last one, move all elements 1 step forward
-                    int j = i+1;
-                    while(j < len){
-                        nums[j-1] = nums[j];
-                        j++;
-                    }
+                    // nums[i] is not the last one, so need move all elements step one forward.
+                    remove(nums,i);
                 }
                 len--;
+            }else {
+                // 只有没有元素移动的情况下i才能增长。因为在arr[i-1]=arr[i]的情况下，新的arr[i]仍然有可能会发生arr[i-1]=arr[i]的情况
+                i++;
             }
-            i++;
         }
 
-        System.out.println("After: " + Arrays.toString(nums));
+        //System.out.println("After: " + Arrays.toString(nums));
         return len;
     }
 
+
+    /**
+     * 双指针，终极解法？
+     * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/solution/shuang-zhi-zhen-shan-chu-zhong-fu-xiang-dai-you-hu/
+     * */
+    public int removeDuplicates1(int[] nums) {
+        return 0;
+    }
+
     public static void main(String[] args){
-        int[] arr = {1,1,2,3,3};
+        //int[] arr = {1,1};
+        //int[] arr = {1,1,2,2};
+
+        // Special case, 3 same elements join together.
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
 
 
         RemoveRepeatedInSortedArray ex = new RemoveRepeatedInSortedArray();
 
-        ex.remove(arr, 1);
+        //ex.remove(arr, 1);
 
-
-        //int len = ex.removeDuplicates(arr);
-        //System.out.println("len=" + len);
+        int len = ex.removeDuplicates(arr);
+        System.out.println("len=" + len);
     }
 
 }
