bdbc8ba635f24abf4fafac8eda873ad3ce7e94e2
B5WANG <neo.b5wang@gmail.com>
2023-02-14 22:40:27 +0800

    Dynamic proxy
    Dynamic proxy

---
+++ src/main/java/com/b5wang/javabasics/alg/primary/linkedlist/DeleteNodeInLinkedList.java
@@ -0,0 +1,19 @@
+package com.b5wang.javabasics.alg.primary.linkedlist;
+
+/**
+ * https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnarn7/
+ * Delete a node in linked list.
+ * */
+public class DeleteNodeInLinkedList {
+
+    public void deleteNode(ListNode node) {
+        do{
+
+        }while(node.next != null);
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/main/java/com/b5wang/javabasics/alg/primary/linkedlist/ListNode.java
@@ -0,0 +1,11 @@
+package com.b5wang.javabasics.alg.primary.linkedlist;
+
+class ListNode {
+    int val;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    ListNode next;
+}
