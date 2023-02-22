840eeb4dfe35e65f4cb9eb16c964da48aebe6582
B5WANG <neo.b5wang@gmail.com>
2023-02-23 00:16:53 +0800

    deleteNode
    deleteNode

--- src/main/java/com/b5wang/javabasics/alg/primary/linkedlist/DeleteNodeInLinkedList.java
+++ src/main/java/com/b5wang/javabasics/alg/primary/linkedlist/DeleteNodeInLinkedList.java
@@ -6,10 +6,13 @@ package com.b5wang.javabasics.alg.primary.linkedlist;
  * */
 public class DeleteNodeInLinkedList {
 
+    /**
+     * 在不知道pre-node的情况下如何删除节点？
+     * 用它的下一个节点把当前要删除的节点覆盖掉就可以了。
+     * */
     public void deleteNode(ListNode node) {
-        do{
-
-        }while(node.next != null);
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
     public static void main(String[] args){
