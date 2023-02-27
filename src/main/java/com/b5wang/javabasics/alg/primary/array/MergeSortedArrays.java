4c0b66af3003a6c9167be559094c4b3e2226e831
B5WANG <neo.b5wang@gmail.com>
2023-02-27 17:55:01 +0800

    Merge 2 sorted arrays
    Merge 2 sorted arrays

--- src/main/java/com/b5wang/javabasics/alg/primary/array/MergeSortedArrays.java
+++ src/main/java/com/b5wang/javabasics/alg/primary/array/MergeSortedArrays.java
@@ -1,5 +1,15 @@
 package com.b5wang.javabasics.alg.primary.array;
 
-/***/
+/**
+ * https://leetcode.cn/leetbook/read/top-interview-questions-easy/xnumcr/
+ *
+ * */
 class MergeSortedArrays {
+
+    static void mert(int[] nums1, int m, int[] nums2, int n){
+
+        
+
+    }
+
 }
