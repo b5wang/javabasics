d015d7ebfdc97508c917c7b02f4f87485a1bcc10
B5WANG <neo.b5wang@gmail.com>
2025-06-24 00:44:15 +0800

    Java 8 Date-Time APIs
    Java 8 Date-Time APIs

--- src/main/java/com/b5wang/javabasics/java/datetimeframe/DateTimeExample.java
+++ src/main/java/com/b5wang/javabasics/java/datetimeframe/DateTimeExample.java
@@ -2,27 +2,62 @@ package com.b5wang.javabasics.java.datetimeframe;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.TimeZone;
 
 public class DateTimeExample {
 
     public static void main(String[] args) {
 
-        // UTC+0
-        Instant instant = Instant.now();
-        System.out.println("instant: " + instant.toEpochMilli());
-        System.out.println("instant: " + instant.toString());
-        System.out.println("instant(atZone): " + instant.atZone(TimeZone.getDefault().toZoneId()).toInstant().toEpochMilli());
-        System.out.println("instant(atZone): " + instant.atZone(TimeZone.getDefault().toZoneId()).toString());
+        /*
+         * 1. Instant.now().toEpochMilli() == System.currentTimeMillis()
+         * 2. In different time zones, the millis are all the same values.
+         * 3. milliseconds time difference between current time and Etc/GMT or Etc/UTC 1970-01-01 00:00:00.000
+         * */
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("New Instant and old System.currentTimeMillis():");
+        Instant nowInstant = Instant.now();
+        long toEpochMilli = nowInstant.toEpochMilli();
+        long currentTimeMillis = System.currentTimeMillis();
+        long toEpochMilliGMT = nowInstant.atZone(ZoneId.of("Etc/GMT")).toInstant().toEpochMilli();
+        long toEpochMilliGMT8 = nowInstant.atZone(ZoneId.of("Etc/GMT+8")).toInstant().toEpochMilli();
+        System.out.println("Instant toEpochMilli    : " + toEpochMilli);
+        System.out.println("System currentTimeMillis: " + currentTimeMillis);
+        System.out.println("Instant GMT             : " + toEpochMilliGMT);
+        System.out.println("Instant GMT+8           : " + toEpochMilliGMT8);
 
-        // LocalDateTime
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Instant:");
+        /*
+         * Instant is milliseconds of UTC, or Etc/GMT, or Etc/UTC. Instant does not have TimeZone information
+         */
+        Instant instant = Instant.now();
+        System.out.println("instant                 : " + instant.toEpochMilli());
+        System.out.println("instant                 : " + instant.toString());
+        System.out.println("instant(defaultTimeZone): " + instant.atZone(TimeZone.getDefault().toZoneId()).toInstant().toEpochMilli());
+        System.out.println("instant(defaultTimeZone): " + instant.atZone(TimeZone.getDefault().toZoneId()).toString());
+        /*
+         * Etc/GMT-8 == GMT+8
+         */
+        System.out.println("instant(Etc/GMT-8)      : " + instant.atZone(ZoneId.of("Etc/GMT-8")).toInstant().toEpochMilli());
+        System.out.println("instant(Etc/GMT-8)      : " + instant.atZone(ZoneId.of("Etc/GMT-8")).toString());
+        System.out.println("instant(GMT+8)          : " + instant.atZone(ZoneId.of("GMT+8")).toInstant().toEpochMilli());
+        System.out.println("instant(GMT+8)          : " + instant.atZone(ZoneId.of("GMT+8")).toString());
+        System.out.println("instant(Etc/GMT+8)      : " + instant.atZone(ZoneId.of("Etc/GMT+8")).toInstant().toEpochMilli());
+        System.out.println("instant(Etc/GMT+8)      : " + instant.atZone(ZoneId.of("Etc/GMT+8")).toString());
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("LocalDateTime:");
+        /*
+         *LocalDateTime has no timezone information
+         */
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println("localDateTime: " + localDateTime.toString());
+        System.out.println("localDateTime           : " + localDateTime.toString());
 
         // ZoneDateTime
         ZonedDateTime zonedDateTime = ZonedDateTime.now();
-        System.out.println("zonedDateTime: " + zonedDateTime.getZone());
-        System.out.println("zonedDateTime: " + zonedDateTime.toString());
+        System.out.println("zonedDateTime           : " + zonedDateTime.getZone());
+        System.out.println("zonedDateTime           : " + zonedDateTime.toString());
         System.out.println("zonedDateTime epochMilli: " + zonedDateTime.toInstant().toEpochMilli());
 
         // TimeZone switch
